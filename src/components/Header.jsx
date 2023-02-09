fcb91eb959194e6f58ad037ad8462456e46d8822
Cesare10-dev <cesare1087@gmail.com>
2023-02-10 00:19:26 +0100

    hamb icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { SiMicrosoft } from "react-icons/si";
 import { BiDotsHorizontalRounded } from "react-icons/bi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxAvatar } from "react-icons/rx";
 
 function Header() {
   return (
@@ -22,7 +24,19 @@ function Header() {
         </ul>
       </div>
 
-      <div></div>
+      <div>
+        <ul className="flex">
+          <li>English</li>
+          <div className="flex items-center">
+            <h2>Sign In</h2>
+            <RxAvatar />
+          </div>
+          <div className="flex items-center">
+            <h2>Rewards</h2>
+            <GiHamburgerMenu />
+          </div>
+        </ul>
+      </div>
     </div>
   );
 }
